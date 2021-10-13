c11a0699e237f38c5f8fabb5587130db21899933
shubhasankar <ssbhaumikdmr@gmail.com>
2021-10-13 19:40:53 +0530

    feat: Added test casses for create get update and delete notes

--- test/note.test.js
+++ test/note.test.js
@@ -1,146 +1,430 @@
-/* eslint-disable node/handle-callback-err */
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const server = require("../server");
 const faker = require("faker");
+const noteInputs = require("./note.test.json");
 
 chai.use(chaiHttp);
-const noteDB = require("./note.test.json");
 chai.should();
 
-describe("create notes api", () => {
-  it("notes", (done) => {
-    const token = noteDB.notes.validToken;
+describe("create note api for positive and negative test case", () => {
+  it("GivenNotesDetails_When_Note_Created_Successfully", (done) => {
+    const token = noteInputs.notes.loginValidToken;
     const createNotes = {
       title: faker.lorem.word(),
       description: faker.lorem.sentence()
     };
-    console.log(createNotes);
     chai
       .request(server)
       .post("/createnotes")
       .set({ authorization: token })
       .send(createNotes)
       .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
         res.should.have.status(201);
         done();
       });
   });
-
-  it("givenCreateNotes_whenInvalidToken_shouldNotbeCreated", (done) => {
-    const token = noteDB.notes.invalidToken;
+  it("GivenNotesDetails_When_Your_Token_Has_Expiered", (done) => {
+    const token = noteInputs.notes.TokenExpiered;
     const createNotes = {
       title: faker.lorem.word(),
       description: faker.lorem.sentence()
     };
-    console.log(createNotes);
     chai
       .request(server)
       .post("/createnotes")
       .set({ authorization: token })
       .send(createNotes)
       .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(401);
+        done();
+      });
+  });
+  it("GivenNotesDetails_When_Your_Title_And_Description_Is_Empty_BadRequest", (done) => {
+    const token = noteInputs.notes.loginValidToken;
+    const createNotes = noteInputs.notes.EmptyTitleAndDis;
+    chai
+      .request(server)
+      .post("/createnotes")
+      .set({ authorization: token })
+      .send(createNotes)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
         res.should.have.status(400);
         done();
       });
   });
 });
 
-// get note test cases
-describe("get notes api", () => {
-  it("notes", (done) => {
-    const token = noteDB.notes.getNoteWithValidToken;
+describe("get all note api for positive and negative test case", () => {
+  it("GivenGetAllNotesDetails_When_Note_Get_Successfully", (done) => {
+    const token = noteInputs.notes.loginValidToken;
     chai
       .request(server)
       .get("/getnotes")
       .set({ authorization: token })
       .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
         res.should.have.status(201);
         done();
       });
   });
-
-  it("givenCreateNotes_whenInvalidToken_shouldNotbeGet", (done) => {
-    const token = noteDB.notes.getNoteWithInValidToken;
+  it("GivenGetAllNotesDetails_When_Token_Has_Expiered", (done) => {
+    const token = noteInputs.notes.TokenExpiered;
     chai
       .request(server)
       .get("/getnotes")
       .set({ authorization: token })
+
       .end((err, res) => {
-        res.should.have.status(400);
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(401);
+        done();
+      });
+  });
+  it("GivenGetAllNotesDetails_When_Token_Has_Empty", (done) => {
+    const token = noteInputs.notes.EmptyToken;
+    chai
+      .request(server)
+      .get("/getnotes")
+      .set({ authorization: token })
+
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(401);
         done();
       });
   });
 });
 
-// update note test cases
-describe("Update notes api", () => {
-  it("givenPoperDetails_ShouldUpdateNote", (done) => {
-    const token = noteDB.notes.getNoteWithValidToken;
-    const note = noteDB.updateNote.validData;
+describe("get note by id api for positive and negative test case", () => {
+  it("GivenGetNoteByIdDetails_When_Note_Get_Successfully", (done) => {
+    const token = noteInputs.notes.loginValidToken;
+    const id = noteInputs.notes.GetById;
     chai
       .request(server)
-      .put("/updatenotes/615ff1878c9fe615f8a35ed8")
+      .get(`/getnotesid/${id}`)
       .set({ authorization: token })
-      .send(note)
       .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(200);
+        done();
+      });
+  });
+  it("GivenGetNoteByIdDetails_When_Token_Has_Expiered", (done) => {
+    const token = noteInputs.notes.TokenExpiered;
+    const id = noteInputs.notes.GetById;
+    chai
+      .request(server)
+      .get(`/getnotesid/${id}`)
+      .set({ authorization: token })
+
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(401);
+        done();
+      });
+  });
+  it("GivenGetNoteByIdDetails_When_Token_Has_Empty", (done) => {
+    const token = noteInputs.notes.EmptyToken;
+    const id = noteInputs.notes.GetById;
+    chai
+      .request(server)
+      .get(`/getnotesid/${id}`)
+      .set({ authorization: token })
+
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(401);
+        done();
+      });
+  });
+  it("GivenGetNoteByIdDetails_When_Token_And_Id_Both_Are_Ok", (done) => {
+    const token = noteInputs.notes.loginValidToken;
+    const id = noteInputs.notes.GetById;
+    chai
+      .request(server)
+      .get(`/getnotesid/${id}`)
+      .set({ authorization: token })
+
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(200);
+        done();
+      });
+  });
+  it("GivenGetNoteByIdDetails_When_Id_Is_Invalid", (done) => {
+    const token = noteInputs.notes.loginValidToken;
+    const id = noteInputs.notes.InvalidId;
+    chai
+      .request(server)
+      .get(`/getnotesid/${id}`)
+      .set({ authorization: token })
+
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(404);
+        done();
+      });
+  });
+  it("GivenGetNoteByIdDetails_When_Id_Has_Empty", (done) => {
+    const token = noteInputs.notes.loginValidToken;
+    const id = noteInputs.notes.EmptyId;
+    chai
+      .request(server)
+      .get(`/getnotesid/${id}`)
+      .set({ authorization: token })
+
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(404);
+        done();
+      });
+  });
+});
+
+describe("update note by id api for positive and negative test case", () => {
+  it("GivenUpdateNoteByIdDetails_When_Note_Get_Successfully", (done) => {
+    const token = noteInputs.notes.loginValidToken;
+    const id = noteInputs.notes.UpdateById;
+    const updateNote = {
+      title: faker.lorem.word(),
+      description: faker.lorem.sentence()
+    };
+    chai
+      .request(server)
+      .put(`/updatenotes/${id}`)
+      .set({ authorization: token })
+      .send(updateNote)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
         res.should.have.status(201);
         done();
       });
   });
-
-  it("givenInvalidToken_ShouldUpdateNote", (done) => {
-    const token = noteDB.notes.getNoteWithInValidToken;
-    const note = noteDB.updateNote.validData;
+  it("GivenUpdateNoteByIdDetails_When_Token_Has_Expiered", (done) => {
+    const token = noteInputs.notes.TokenExpiered;
+    const id = noteInputs.notes.UpdateById;
+    const updateNote = {
+      title: faker.lorem.word(),
+      description: faker.lorem.sentence()
+    };
     chai
       .request(server)
-      .put("/updatenotes/6163a92b4ec773015a13abb0")
+      .put(`/updatenotes/${id}`)
       .set({ authorization: token })
-      .send(note)
+      .send(updateNote)
       .end((err, res) => {
-        res.should.have.status(400);
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(401);
+        done();
+      });
+  });
+  it("GivenUpdateNoteByIdDetails_When_Token_Has_Empty", (done) => {
+    const token = noteInputs.notes.EmptyToken;
+    const id = noteInputs.notes.UpdateById;
+    const updateNote = {
+      title: faker.lorem.word(),
+      description: faker.lorem.sentence()
+    };
+    chai
+      .request(server)
+      .put(`/updatenotes/${id}`)
+      .set({ authorization: token })
+      .send(updateNote)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(401);
+        done();
+      });
+  });
+  it("GivenUpdateNoteByIdDetails_When_Token_And_Id_Both_Are_Ok", (done) => {
+    const token = noteInputs.notes.loginValidToken;
+    const id = noteInputs.notes.UpdateById;
+    const updateNote = {
+      title: faker.lorem.word(),
+      description: faker.lorem.sentence()
+    };
+    chai
+      .request(server)
+      .put(`/updatenotes/${id}`)
+      .set({ authorization: token })
+      .send(updateNote)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(201);
+        done();
+      });
+  });
+  it("GivenUpdateNoteByIdDetails_When_Id_Is_Invalid", (done) => {
+    const token = noteInputs.notes.loginValidToken;
+    const id = noteInputs.notes.UpdateById_InvalidId;
+    const updateNote = {
+      title: faker.lorem.word(),
+      description: faker.lorem.sentence()
+    };
+    chai
+      .request(server)
+      .put(`/updatenotes/${id}`)
+      .set({ authorization: token })
+      .send(updateNote)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(201);
+        done();
+      });
+  });
+  it("GivenUpdateNoteByIdDetails_When_Id_Has_Empty", (done) => {
+    const token = noteInputs.notes.loginValidToken;
+    const id = noteInputs.notes.UpdateById_EmptyId;
+    const updateNote = {
+      title: faker.lorem.word(),
+      description: faker.lorem.sentence()
+    };
+    chai
+      .request(server)
+      .put(`/updatenotes/${id}`)
+      .set({ authorization: token })
+      .send(updateNote)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(404);
         done();
       });
   });
 });
 
-// delete note test cases
-describe("delete notes api", () => {
-  it("givenPoperDetails_ShouldDeleteNote", (done) => {
-    const token = noteDB.notes.getNoteWithValidToken;
+describe("delete note by id api for positive and negative test case", () => {
+  it("GivenDeleteNoteByIdDetails_When_Note_Deleted_Successfully", (done) => {
+    const token = noteInputs.notes.loginValidToken;
+    const id = noteInputs.notes.DeleteById;
     chai
       .request(server)
-      .delete("/deletenotes/615ff1878c9fe615f8a35ed8")
+      .delete(`/deletenotes/${id}`)
       .set({ authorization: token })
       .end((err, res) => {
-        res.should.have.status(200);
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(500);
         done();
       });
   });
-
-  it("givenInvalidToken_ShouldUpdateNote", (done) => {
-    const token = noteDB.notes.getNoteWithInValidToken;
+  it("GivenGetNoteByIdDetails_When_Token_Has_Expiered", (done) => {
+    const token = noteInputs.notes.TokenExpiered;
+    const id = noteInputs.notes.DeleteById;
     chai
       .request(server)
-      .delete("/deletenotes/6165c20f245abe3011a5ae51")
+      .delete(`/deletenotes/${id}`)
       .set({ authorization: token })
+
       .end((err, res) => {
-        res.should.have.status(400);
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(401);
         done();
       });
   });
-});
-
-// get data by id
-describe("Get notes by ID api", () => {
-  it("givenPoperDetails_ShouldGetNote", (done) => {
-    const token = noteDB.notes.getNoteWithValidToken;
+  it("GivenGetNoteByIdDetails_When_Token_Has_Empty", (done) => {
+    const token = noteInputs.notes.EmptyToken;
+    const id = noteInputs.notes.DeleteById;
     chai
       .request(server)
-      .get("/getnotes/615ff1878c9fe615f8a35ed8")
+      .delete(`/deletenotes/${id}`)
       .set({ authorization: token })
+
       .end((err, res) => {
-        res.should.have.status(200);
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(401);
+        done();
+      });
+  });
+  it("GivenGetNoteByIdDetails_When_Token_And_Id_Both_Are_Ok", (done) => {
+    const token = noteInputs.notes.loginValidToken;
+    const id = noteInputs.notes.DeleteById;
+    chai
+      .request(server)
+      .delete(`/deletenotes/${id}`)
+      .set({ authorization: token })
+
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(500);
+        done();
+      });
+  });
+  it("GivenGetNoteByIdDetails_When_Id_Is_Invalid", (done) => {
+    const token = noteInputs.notes.loginValidToken;
+    const id = noteInputs.notes.DeleteById_InvalidId;
+    chai
+      .request(server)
+      .delete(`/deletenotes/${id}`)
+      .set({ authorization: token })
+
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(500);
+        done();
+      });
+  });
+  it("GivenGetNoteByIdDetails_When_Id_Has_Empty", (done) => {
+    const token = noteInputs.notes.loginValidToken;
+    const id = noteInputs.notes.DeleteById_EmptyId;
+    chai
+      .request(server)
+      .delete(`/deletenotes/${id}`)
+      .set({ authorization: token })
+
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(404);
         done();
       });
   });
